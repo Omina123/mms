0b92b70d385a63123924f873ffdc1af12509a660
Caleb <Caleb@JUMBE.FIN_SERVER.LOCAL>
2026-05-15 17:12:57 +0300

    first commit

--- Home/urls.py
+++ Home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from Home.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('about/', About, name='about'),
     path('services/', Services, name='services'),  
